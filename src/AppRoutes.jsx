bc1c36a6af21d1e1c3a562a42cb42b7b0ee5215d
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-01-05 15:52:45 -0300

    commit 6

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import Inicio from "pages/Inicio";
+import Favoritos from "pages/Favoritos";
+import Player from "pages/Player";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<Inicio />} />
+          <Route path="favoritos" element={<Favoritos />} />
+          <Route path=":id" element={<Player />} />
+          <Route path=":id" element={<Player />} />
+          <Route path="*" element={<NaoEncontrada />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
